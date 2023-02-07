f3aba106d17d87b1d1d73bd713295dd449d6f4aa
Mohammed Alhadi <cls>
2023-02-07 18:46:34 +0300

    sort method added by A

--- Sorter.java
+++ Sorter.java
@@ -16,16 +16,12 @@ public class Sorter {
             arr[i] = temp;
         }
     }
-    void sort(int arr[])
+    void insertionSort(int arr[])
     {
         int n = arr.length;
         for (int i = 1; i < n; ++i) {
             int key = arr[i];
             int j = i - 1;
- 
-            /* Move elements of arr[0..i-1], that are
-               greater than key, to one position ahead
-               of their current position */
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
@@ -33,14 +29,8 @@ public class Sorter {
             arr[j + 1] = key;
         }
     }
- 
-    /* A utility function to print array of size n*/
-    static void printArray(int arr[])
-    {
-        int n = arr.length;
-        for (int i = 0; i < n; ++i)
-            System.out.print(arr[i] + " ");
- 
-        System.out.println();
+    public void sort(int[] arr){
+        selectionSort(arr);
     }
+ 
 }
